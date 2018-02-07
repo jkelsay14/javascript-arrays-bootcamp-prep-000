1494d59fa97ef6005ef3e6f12da96ddfb576fa96
Jared Kelsay <jaredkelsay@gmail.com>
2018-02-07 17:44:02 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+function addElementToBeginningOfArray(array, element) {
+  array = (chocolateBars)
+  element = ('foo')
+chocolateBars[1] = ['foo']
+return chocolateBars
+}
